62caa1c5be6814d438c95916da01b188c25610f1
Reizu Seharu <reizu.seharu@gmail.com>
2021-03-07 18:07:04 -0500

    {enhancement} Two column form

--- src/views/InputForm.tsx
+++ src/views/InputForm.tsx
@@ -44,6 +44,9 @@ const onSubmit = async (values) => {
 }
 const validate = (values) => {
   const errors = {}
+  if (!values.username) {
+    errors.username = 'Required'
+  }
   if (!values.pokemon) {
     errors.pokemon = 'Required'
   }
@@ -84,12 +87,9 @@ export function InputForm() {
     }}
     >
     <Navbar/>
-    <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
+    <div style={{ padding: 16, margin: 'auto', maxWidth: "80%" }}>
       <CssBaseline />
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
-        Pokémon Snap
-      </Typography>
-      <Typography variant="h5" align="center" component="h2" gutterBottom>
+      <Typography variant="h5" align="center" component="h1" gutterBottom>
         High Score Submission
       </Typography>
       <Form
@@ -102,108 +102,111 @@ export function InputForm() {
         }}
         validate={validate}
         render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
+          <form onSubmit={handleSubmit} noValidate
+                style={{backgroundColor: "#000000", opacity: 0.7}}>
             <Paper style={{ padding: 16 }}>
-              <Grid container alignItems="flex-start" spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    name="username"
-                    fullWidth
-                    required
-                    type="text"
-                    label="Username"
-                  />
-                </Grid>
-                <Grid item xs={7}>
-                  <Autocomplete
-                    name="pokemon"
-                    label="Pokemon"
-                    multiple={false}
-                    required={true}
-                    options={options.sort(
-                      (a, b) => -b.stage.localeCompare(a.stage),
-                    )}
-                    groupBy={(option) => option.stage}
-                    getOptionLabel={(option) => option.name}
-                    getOptionValue={(option) => option.name}
-                    style={{ width: 200 }}
-                    renderOption={(option) => option.name}
-                  />
-                </Grid>
-                <Grid item xs={5}>
-                  <TextField
-                    fullWidth
-                    required
-                    name="score"
-                    type="number"
-                    label="Score"
-                  />
-                </Grid>
-                <Grid container spacing={0} alignItems="center">
-                  <Grid item>
-                    <Radios
-                      label="Region"
-                      name="region"
-                      required={true}
-                      data={[
-                        { label: 'NTSC-USA', value: 'NTSC-USA' },
-                        { label: 'NTSC-JPN', value: 'NTSC-JPN' },
-                        { label: 'PAL', value: 'PAL' },
-                        { label: 'PAL-M', value: 'PAL-M' },
-                      ]}
-                      radioGroupProps={{ row: true }}
-                    />
+              <Grid container>
+                <Grid container xs={6} direction="column">
+                  <Grid xs={12} container alignItems="flex-start" spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        name="username"
+                        fullWidth
+                        required
+                        type="text"
+                        label="Username"
+                      />
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Autocomplete
+                        name="pokemon"
+                        label="Pokemon"
+                        multiple={false}
+                        required={true}
+                        options={options.sort(
+                          (a, b) => -b.stage.localeCompare(a.stage),
+                        )}
+                        groupBy={(option) => option.stage}
+                        getOptionLabel={(option) => option.name}
+                        getOptionValue={(option) => option.name}
+                        style={{ width: 200 }}
+                        renderOption={(option) => option.name}
+                      />
+                    </Grid>
+                    <Grid item xs={5}>
+                      <TextField
+                        fullWidth
+                        required
+                        name="score"
+                        type="number"
+                        label="Score"
+                      />
+                    </Grid>
+                    <Grid item xs={10}>
+                      <TextField name="videoLink" label="Video Link" />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
+                      <label htmlFor="icon-button-file">
+                        <IconButton color="primary" aria-label="upload picture" component="span">
+                          <PhotoCamera />
+                        </IconButton>
+                      </label>
+                    </Grid>
                   </Grid>
                 </Grid>
-                <Grid container spacing={0} alignItems="center">
-                  <Grid item xs={9}>
-                    <Radios
-                      label="Console"
-                      name="console"
-                      required={true}
-                      data={[
-                        { label: 'Nintendo 64', value: 'N64' },
-                        { label: 'Wii VC', value: 'Wii VC' },
-                        { label: 'Wii U VC', value: 'Wii U VC' },
-                      ]}
-                      radioGroupProps={{ row: true }}
-                    />
+                <Grid container xs={6} direction="column">
+                  <Grid container spacing={0} alignItems="center">
+                    <Grid item>
+                      <Radios
+                        label="Region"
+                        name="region"
+                        required={true}
+                        data={[
+                          { label: 'NTSC-USA', value: 'NTSC-USA' },
+                          { label: 'NTSC-JPN', value: 'NTSC-JPN' },
+                          { label: 'PAL', value: 'PAL' },
+                          { label: 'PAL-M', value: 'PAL-M' },
+                        ]}
+                        radioGroupProps={{ row: true }}
+                      />
+                    </Grid>
                   </Grid>
-                </Grid>
-                <Grid container spacing={0} alignItems="center">
-                  <Grid item xs={3}>
-                    <Checkboxes
-                      label=""
-                      name="isEmulated"
-                      required={true}
-                      data={[{ label: 'Emulated', value: 'isEmulated' }]}
-                    />
+                  <Grid container spacing={1}>
+                    <Grid item>
+                      <Radios
+                        label="Console"
+                        name="console"
+                        required={true}
+                        data={[
+                          { label: 'Nintendo 64', value: 'N64' },
+                          { label: 'Wii VC', value: 'Wii VC' },
+                          { label: 'Wii U VC', value: 'Wii U VC' },
+                        ]}
+                        radioGroupProps={{ row: true }}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Checkboxes
+                        label=""
+                        name="isEmulated"
+                        required={true}
+                        data={[{ label: 'Emulated', value: 'isEmulated' }]}
+                      />
+                    </Grid>
                   </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                    <TextField name="videoLink" label="Video Link" />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-                    <label htmlFor="icon-button-file">
-                      <IconButton color="primary" aria-label="upload picture" component="span">
-                        <PhotoCamera />
-                      </IconButton>
-                    </label>
-                  </Grid>
-                </Grid>
-                <Grid container spacing={0} alignItems="center">
-                  <Grid item xs={6}>
-                    <DatePicker
-                      label="Picture/Video Taken On"
-                      name="takenOn"
-                      required={true}
-                      dateFunsUtils={DateFnsUtils}
-                      margin="normal"
-                      variant="inline"
-                      format="yyyy-MM-dd"
-                    />
+                  <Grid container spacing={0} alignItems="center">
+                    <Grid item xs={12}>
+                      <DatePicker
+                        label="Picture/Video Taken On"
+                        name="takenOn"
+                        required={true}
+                        dateFunsUtils={DateFnsUtils}
+                        margin="normal"
+                        variant="inline"
+                        format="yyyy-MM-dd"
+                      />
+                    </Grid>
                   </Grid>
                 </Grid>
                 <Grid item xs={12}>
